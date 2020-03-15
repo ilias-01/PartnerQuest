37e117db092a0bd3e11ca5a9008275190ac91328
unknown <21911226@etu.unicaen.fr>
2020-03-15 15:18:16 +0100

    2 share

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
 set_include_path("./src");
-//require_once('/users/21911226/private/mysql_config.php');
 require_once ("Router.php");
 require_once ("model/QuizStorageMySQL.php");
 require_once("model/AccountStorageMySQL.php");
 require_once("model/AuthenticationManager.php");
 
-$user="21706533";
-$pass="zavoot5ijiTh2Ail";
-$dbname="21706533_3";
-$dbhost="mysql.info.unicaen.fr:3306";
 
-$pdo=new PDO('mysql:host=mysql.info.unicaen.fr:3306;dbname=21706533_3', $user, $pass);
+/** LOCAL DATABASE !!!*/
 
-//$pdo=new PDO("mysql:host=".MYSQL_HOST.";dbname=".MYSQL_DB."", MYSQL_USER, MYSQL_PASSWORD);
+$user="root";
+$pass="";
+$pdo=new PDO("mysql:host=localhost;dbname=partnerquest", $user, $pass);
+
+/** END ------- */
+
+
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
